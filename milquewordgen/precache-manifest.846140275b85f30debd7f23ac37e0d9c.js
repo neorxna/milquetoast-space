9fa5aa6e9bde7326a6a01d1b8becb14c8fedb04d
Arron M <amcl080@aucklanduni.ac.nz>
2020-01-06 14:54:37 +1300

    milquewordgen 0.1

---
+++ milquewordgen/precache-manifest.846140275b85f30debd7f23ac37e0d9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305ce25878978eeb4fefebe6e8f2c149",
+    "url": "/milquewordgen/index.html"
+  },
+  {
+    "revision": "d3e996ef00b862f1fa6c",
+    "url": "/milquewordgen/static/css/main.970f23f5.chunk.css"
+  },
+  {
+    "revision": "0fccd761ab72047b2fb3",
+    "url": "/milquewordgen/static/js/2.3d37267c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/milquewordgen/static/js/2.3d37267c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3e996ef00b862f1fa6c",
+    "url": "/milquewordgen/static/js/main.e1a51ab4.chunk.js"
+  },
+  {
+    "revision": "8b833d9385057ffbca3d",
+    "url": "/milquewordgen/static/js/runtime-main.0cca9136.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/milquewordgen/static/media/logo.5d5d9eef.svg"
+  }
+]);
