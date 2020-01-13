3d1d00ec977047cd592ae25fa6a4fac6115170f3
Arron M <amcl080@aucklanduni.ac.nz>
2020-01-13 22:32:35 +1300

    scattershell islands

---
+++ scattershell-islands/precache-manifest.81e03ad91d4ed1f2497de503c98fa430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05506f3792764f08f4fb49162390b2b3",
+    "url": "/scattershell-islands/index.html"
+  },
+  {
+    "revision": "ac60150f05d062e211fd",
+    "url": "/scattershell-islands/static/css/main.a050575d.chunk.css"
+  },
+  {
+    "revision": "cc01f47bde381e1b132c",
+    "url": "/scattershell-islands/static/js/2.2d4a3e50.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/scattershell-islands/static/js/2.2d4a3e50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac60150f05d062e211fd",
+    "url": "/scattershell-islands/static/js/main.9f935efd.chunk.js"
+  },
+  {
+    "revision": "09114844e6f4b53b2546",
+    "url": "/scattershell-islands/static/js/runtime-main.45f97f40.js"
+  }
+]);
