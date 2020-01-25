2120f4c9099c7bea8daef96acfb7d4ed5546827b
Arron M <amcl080@aucklanduni.ac.nz>
2020-01-25 15:10:46 +1300

    craig's oregano 0.1

---
+++ craigs-oregano/precache-manifest.8b982f388d794b97364b26569aebba47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2749b9d6c7f3712adbc18a2b3f49fd",
+    "url": "/craigs-oregano/index.html"
+  },
+  {
+    "revision": "de4ea0b460a291c76b52",
+    "url": "/craigs-oregano/static/css/main.8d8bda3b.chunk.css"
+  },
+  {
+    "revision": "1e61ec1276d180fb1d49",
+    "url": "/craigs-oregano/static/js/2.79b06b36.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/craigs-oregano/static/js/2.79b06b36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de4ea0b460a291c76b52",
+    "url": "/craigs-oregano/static/js/main.7e58e385.chunk.js"
+  },
+  {
+    "revision": "2fc6be9f832890dbf52b",
+    "url": "/craigs-oregano/static/js/runtime-main.7ca92bec.js"
+  }
+]);
